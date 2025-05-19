2073d6d1345a5ab3d1efaab4276e977c2556d9c0
jinyung <wongjinyung@gmail.com>
2025-05-19 14:04:06 +0800

    make simpler

--- Kmer_analysis.py
+++ Kmer_analysis.py
@@ -4,39 +4,29 @@ import plotly.express as px
 
 st.title("K-mer Analysis")
 
-# Input sequence
+# get inputs
 use_sample = st.sidebar.checkbox("Use sample sequence", value=True)
-default_seq = "GATCACAGGTCTATCACCCTATTAACCACTCACGGGAGCTCTCCATGCATTTGGTATTTTCGTCTGGGGGGTATGCACGCGATAGCATTGCGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTGTCTTTGATTC"
-sequence = st.text_area("Enter your sequence:", default_seq if use_sample else "").upper()
-
-# K-mer size
+if use_sample:
+	default = "GATCACAGGTCTATCACCCTATTAACCACTCACGGGAGCTCTCCATGCATTTGGTATTTTCGTC"
+else:
+	default = ""
+sequence = st.text_area("Enter your sequence:", default).upper()
 k = st.sidebar.slider("Select k-mer size:", 2, 5, 3)
 
 if sequence:
-	# Generate k-mers using sliding window
+	# compute, count and sort k-mers
 	kmers = [sequence[i:i+k] for i in range(len(sequence)-k+1)]
-
-	# Count k-mers
 	kmer_counts = Counter(kmers)
-
-	# Sort k-mers by frequency in descending order
 	sorted_items = sorted(kmer_counts.items(), key=lambda x: x[1])
 	kmers, counts = zip(*sorted_items)  # unzip  into two lists
-
-	# Display results
-	st.write(f"**Found {len(kmer_counts)} unique {k}-mers:**")
-
-	# Dynamic bar chart height depending on kmers number
-	dynamic_height = len(kmer_counts) * 20 + 100
-
-	# Create bar chart
-	fig = px.bar(
-		x=counts,
-		y=kmers,
-		title=f"{k}-mer Frequencies",
-		orientation='h',  # makes it horizontal
-		labels={'x': 'Frequency', 'y': 'k-mer'},  # rename axes
-		height=dynamic_height
+	
+	# output
+	st.success(f"Found {len(kmer_counts)} unique {k}-mers:")
+	fig = px.bar(x=counts, y=kmers, title=f"{k}-mer Frequencies",
+		labels={'x': 'Frequency', 'y': 'k-mer'}, 
+		height=len(kmer_counts) * 20 + 100,  # dynamic height
+		orientation = 'h'  # horizontal bar
 	)
-
 	st.plotly_chart(fig)
+else:
+	st.warning("Please enter sequence or use sample sequence")
